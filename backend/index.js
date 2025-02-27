cdca18c9273df54cd2f0a57bec4f2a28893cf62e
Carlos <dschworld@gmail.com>
2025-02-26 23:57:04 -0400

    started css...

--- backend/index.js
+++ backend/index.js
@@ -10,13 +10,23 @@ const booksRoutes = require('./routes/books.routes');
 const request = require('http');
 
 
-app.get('/', (req, res) => {
-    res.send('Yes, it works!');
-})
 
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'uploads')));
+
+app.use(express.static(path.join(__dirname, '../frontend/public'))); // express.static para archivos que no se actualizan como css, js, images...
+app.set('views', path.join(__dirname, '../frontend/views')); // no se usa express.static porque los ejs se actualizan y se deben renderizar
 app.set('view engine', 'ejs');
+
+// const frontendPublicPath = path.join(__dirname, '../frontend/public');
+// const frontendViewsPath = path.join(__dirname, '../frontend/views');
+
+// app.use(express.static(frontendPublicPath));
+// app.set('views', frontendViewsPath);
+// app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index'); // renderiza el archivo index.ejs
+})
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
